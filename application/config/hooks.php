e94e84dc341ec951608f5e67363b89fb390a5099
Stéphane Dion <divers@sdion.net>
2012-02-22 04:18:31 +0100

    -

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,4 +1,14 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Hooks
+| -------------------------------------------------------------------------
+| This file lets you define "hooks" to extend CI without hacking the core
+| files.  Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/general/hooks.html
+|
+*/
 
 $hook['post_controller_constructor'][] = array(
 'class'    => 'axipi_hook',
@@ -15,3 +25,6 @@ $hook['post_controller'][] = array(
 'filepath' => 'hooks',
 'params'   => array()
 );
+
+/* End of file hooks.php */
+/* Location: ./application/config/hooks.php */
