7912b6d431f97bcd28ee0aa3cd49bfa71bfbf303
Stéphane Dion <stephane.dion@brand-advocate.com>
2012-03-16 17:31:23 +0100

    -

--- user_guide/nav/nav.js
+++ user_guide/nav/nav.js
@@ -11,11 +11,33 @@ function create_menu(basepath)
 		'<li><a href="'+base+'toc.html">Table of Contents Page</a></li>' +
 		'</ul>' +
 
-		'</td><td class="td_sep" valign="top">' +
+		'<h3>Basic Info</h3>' +
+		'<ul>' +
+		'<li><a href="'+base+'changes.html">Changes on CodeIgniter</a></li>' +
+		'<li><a href="'+base+'installation.html">Installation</a></li>' +
+		'</ul>' +
 
 		'</td><td class="td_sep" valign="top">' +
 
+		'<h3>General Topics</h3>' +
+		'<ul>' +
+		'<li><a href="'+base+'general/layouts.html">Layouts</a></li>' +
+		'<li><a href="'+base+'general/zones.html">Zones</a></li>' +
+		'</ul>' +
+
 		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Class Reference</h3>' +
+		'<ul>' +
+		'<li><a href="'+base+'libraries/axipi_library.html">axipi Class</a></li>' +
+		'</ul>' +
+
+		'</td><td class="td_sep" valign="top">' +
+
+		'<h3>Helper Reference</h3>' +
+		'<ul>' +
+		'<li><a href="'+base+'helpers/axipi_helper.html">axipi Helper</a></li>' +
+		'</ul>' +
 
 		'</td></tr></table>');
 }
