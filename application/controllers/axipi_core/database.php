1745cc597257a504b4625f839b44fed1f646f5a2
Stéphane Dion <divers@sdion.net>
2012-05-17 06:19:46 +0200

    -

--- application/controllers/axipi_core/database.php
+++ application/controllers/axipi_core/database.php
@@ -45,7 +45,7 @@ class database extends CI_Controller {
 				$rows = $row->Rows;
 			//}
 			$output .= '<tr>';
-			$output .= '<td>'.$row->Name.'</td>';
+			$output .= '<td><a href="'.base_url().'axipi/core/database/_show/'.$row->Name.'">'.$row->Name.'</a></td>';
 			$output .= '<td>'.$row->Comment.'</td>';
 			$output .= '<td>'.$engine.'</td>';
 			$output .= '<td>'.$row->Collation.'</td>';
@@ -55,7 +55,10 @@ class database extends CI_Controller {
 			$output .= '<td>'.convert_size($row->Index_length).'</td>';
 			$output .= '<td>'.$row->Create_time.'</td>';
 			$output .= '<td>'.$row->Update_time.'</td>';
-			//$output .= '<th><a href="index.php?p='.$this->get['p'].'&amp;a=convert-utf8-table&amp;table='.$row->Name.'">Convert to UTF-8</a><a href="index.php?p='.$this->get['p'].'&amp;a=show&amp;table='.$row->Name.'">Voir</a></th>';
+			$output .= '<th>';
+			//$output .= '<a href="index.php?p='.$this->get['p'].'&amp;a=convert-utf8-table&amp;table='.$row->Name.'">Convert to UTF-8</a>';
+			$output .= '<a href="'.base_url().'axipi/core/database/_show/'.$row->Name.'">Show</a>';
+			$output .= '</th>';
 			$output .= '</tr>';
 		}
 		$output .= '</tbody>';
@@ -65,6 +68,48 @@ class database extends CI_Controller {
 		$output .= '</div>';
 
 		$this->zones['content'] = $output;
+	}
+	public function show($table) {
+		$output = '';
+		$query = $this->db->query('SHOW FULL COLUMNS FROM '.$table);
+		if($query->num_rows() > 0) {
+			$query_status = $this->db->query('SHOW TABLE STATUS WHERE name = ?', array($table));
+			$output .= '<div class="box1">
+			<h1>'.$table.' / '.$query_status->row()->Comment.'</h1>
+			<ul>';
+			$output .= '<li><a href="'.base_url().$this->itm->itm_code.'">Index</a></li>';
+			$output .= '</ul>';
+			$output .= '<div class="display">';
+			$output .= '<table>
+			<thead>
+			<tr>
+			<th class="first">Field</th>
+			<th>Type</th>
+			<th>Collation</th>
+			<th>Null</th>
+			<th>Key</th>
+			<th>Default</th>
+			<th>Extra</th>
+			</tr>
+			</thead>';
+			$output .= '<tbody>';
+			foreach($query->result() as $row) {
+				$output .= '<tr>
+				<td>'.$row->Field.'</td>
+				<td>'.$row->Type.'</td>
+				<td>'.$row->Collation.'</td>
+				<td>'.$row->Null.'</td>
+				<td>'.$row->Key.'</td>
+				<td>'.$row->Default.'</td>
+				<td>'.$row->Extra.'</td></tr>';
+			}
+			$output .= '</tbody>';
+			$output .= '</table>';
+			$output .= '</div>
+			</div>';
+		}
+		$this->zones['content'] = $output;
+		//$this->get['a'] = 'list';
 	}
 	public function translation() {
 		$this->load->language('axipi_dynamic');
